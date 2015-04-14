419903de5e6f054bf851ca2d5e28f9a4b72ac8ec
Romain Quilliot <romain.addweb@gmail.com>
2015-04-14 18:52:43 +0200

    Refactoring de Routing

--- app_dev/src/Libraries/Routing.js
+++ app_dev/src/Libraries/Routing.js
@@ -1,77 +1,77 @@
 var Routing = {
     url: [],
     events: [],
+};
 
 
-    /**
-     *
-     * Ajoute une route
-     * @param string name
-     * @param function callback
-     * @param boolean isUrl
-     *
-     */
-    add: function (name, callback, isUrl) {
-        if (!isUrl) {
-            this.events[name] = callback;
+/**
+ *
+ * Ajoute une route
+ * @param string name
+ * @param function callback
+ * @param boolean isUrl
+ *
+ */
+Routing.add = function (name, callback, isUrl) {
+    if (!isUrl) {
+        this.events[name] = callback;
+        return true;
+    }
+
+    this.url[name] = callback;
+    return true;
+};
+
+
+/**
+ *
+ * Recupere le callback d'une page
+ * @param string page
+ * @return mixed
+ *
+ */
+Routing.get = function (page) {
+    return (!this.events[page]) ? false : this.events[page];
+};
+
+
+/**
+ *
+ * Verifie les routes et trouve les correspondances
+ * @return bool
+ *
+ */
+Routing.check = function () {
+    var uri = (window.location.hash).replace('#', '');
+
+    for (schema in this.url) {
+        var matchResult = uri.match('^'+ schema +'$');
+
+        if (matchResult) {
+            var parameters = [];
+
+            for (var i = 1; i < matchResult.length; i++) {
+                parameters[i - 1] = matchResult[i];
+            }
+
+            Routing.url[schema](parameters);
             return true;
         }
-
-        this.url[name] = callback;
-        return true;
-    },
-
-
-    /**
-     *
-     * Recupere le callback d'une page
-     * @param string page
-     * @return mixed
-     *
-     */
-    get: function (page) {
-        return (!this.events[page]) ? false : this.events[page];
-    },
-
-
-    /**
-     *
-     * Verifie les routes et trouve les correspondances
-     * @return bool
-     *
-     */
-    check: function () {
-        var uri = (window.location.hash).replace('#', '');
-
-        for (schema in this.url) {
-            var matchResult = uri.match('^'+ schema +'$');
-
-            if (matchResult) {
-                var parameters = [];
-
-                for (var i = 1; i < matchResult.length; i++) {
-                    parameters[i - 1] = matchResult[i];
-                }
-
-                Routing.url[schema](parameters);
-                return true;
-            }
-        }
-
-        return false;
-    },
-
-
-    /**
-     *
-     * Lance le routeur
-     *
-     */
-    run: function () {
-        Routing.check();
-
-        $(window).on('hashchange', function () {
-            Routing.check();
-        });
     }
+
+    return false;
+};
+
+
+/**
+ *
+ * Lance le routeur
+ *
+ */
+Routing.run = function () {
+    Routing.check();
+
+    $(window).on('hashchange', function () {
+        Routing.check();
+    });
 };
