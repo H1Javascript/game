2f67613ccf1d94ff577de5db4dc7d5bf6fededda
Romain Quilliot <romain.addweb@gmail.com>
2015-04-14 18:52:34 +0200

    Ajout de l'interface Pages

---
+++ app_dev/src/Libraries/Pages.js
@@ -0,0 +1,77 @@
+var Pages = {
+    templatePath: "",
+    params: {},
+};
+
+
+/**
+ *
+ * Definie le path vers les templates
+ * @param string path
+ *
+ */
+Pages.setTemplatePath = function (path) {
+    this.templatePath = path;
+};
+
+
+/**
+ *
+ * Recupere le contenue non compile d'une page
+ * @param string template
+ * @param function callback
+ *
+ */
+Pages.load = function (template, callback) {
+    $.get(this.templatePath +"/"+ template +".hbs", function (content) {
+        callback(content);
+    });
+};
+
+
+/**
+ *
+ * Recupere la page et l'inclue dans le container
+ * @param string page
+ * @param object container
+ *
+ */
+Pages.display = function (page, container) {
+    this.load(page, function (uncompiledContent) {
+        var View = new ViewInterface(uncompiledContent, Handlebars);
+        View.setParam(Pages.params);
+        View.compile();
+
+        container.html(View.getCompiled());
+        Pages.resetParams();
+    });
+};
+
+
+/**
+ *
+ * Definie les variables a passer dans la vue
+ * @param string/object key
+ * @param string value
+ * @return boolean
+ *
+ */
+Pages.setParam = function (key, value) {
+    if (typeof key == 'object') {
+        this.params = key;
+        return true;
+    }
+
+    this.params[key] = value;
+    return true;
+};
+
+
+/**
+ *
+ * Reset les parametres a passer dans la vue
+ *
+ */
+Pages.resetParams = function () {
+    this.params = {};
+};
