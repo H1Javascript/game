2995580c4fac1fa939c265c0ef4aa46de0674c13
Romain Quilliot <romain.addweb@gmail.com>
2015-04-14 19:25:39 +0200

    Ajout du music Controller

---
+++ app_dev/src/Controllers/musicController.js
@@ -0,0 +1,48 @@
+/**
+ *
+ * Charge une musique
+ * @param string music
+ *
+ */
+var musicControllerInterface = function (music) {
+    var musicElement = $('<audio src="'+ music +'">');
+    $('#musics').html('');
+    $('#musics').append(musicElement);
+
+    this.musicElement = $('#musics audio').get(0);
+};
+
+
+/**
+ *
+ * Joue la musique
+ *
+ */
+musicControllerInterface.prototype.play = function () {
+    this.musicElement.play();
+};
+
+
+/**
+ *
+ * Recupere le temps actuelle de jeu de la musique (en seconde)
+ * @return float
+ *
+ */
+musicControllerInterface.prototype.getTime = function () {
+    var currentTime = this.musicElement.currentTime +"";
+    currentTime = currentTime.split('.');
+    currentTime = currentTime[0] +"."+ currentTime[1][0];
+
+    return parseFloat(currentTime);
+};
+
+
+/**
+ *
+ * Met en pause la musique
+ *
+ */
+musicControllerInterface.prototype.pause = function () {
+    this.musicElement.pause();
+};
